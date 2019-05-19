7e52f19ac0463dc6aca09fa8db0670dfd916312f
Thanh Linh <nguyenlethanhlinh95@gmail.com>
2019-05-19 12:37:26 +0700

    hoan thanh page contact

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -27,6 +29,26 @@ class LoginController extends Controller
      */
     protected $redirectTo = '/home';
 
+    protected function home()
+    {
+        // User role
+//        $role = Auth::user()->admin;
+//
+//        switch ($role)
+//        {
+//            case 0:
+//                return '/'; // redirect to home page
+//                break;
+//            case 1:
+//                return '/admin'; // redirect to admin page
+//                break;
+//            default:
+//                return '/login'; // redirect to login page
+//                break;
+//        }
+        return view('front.pages.home');
+    }
+
     /**
      * Create a new controller instance.
      *
@@ -35,5 +57,11 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    // logout
+    public function logout(Request $request) {
+        Auth::logout();
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/about', function (){
     return view('front.pages.about');
 });
 
+Route::get('/contact', function (){
+    return view('front.pages.contact');
+})->name('contact');
+
 /*
  * Route Shop
  * */
@@ -28,5 +32,5 @@ Route::get('/shop','HomeController@shop');
  * End Route Shop
  * */
 Auth::routes();
-
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 Route::get('/home', 'HomeController@index')->name('home');
